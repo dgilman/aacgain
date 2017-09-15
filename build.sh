57b8942ef178dbb64afbd778d9c5e24195f7a1b9
David Gilman <davidgilman1@gmail.com>
2017-09-15 19:37:52 -0400

    Tell tar how tarball is compressed

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ AACGAIN_ROOT=$(pwd)
 
 echo "travis_fold:start:mp4v2"
 mkdir "$AACGAIN_ROOT/mp4v2" && cd "$AACGAIN_ROOT/mp4v2"
-wget -O - https://github.com/TechSmith/mp4v2/archive/2.1.0.tar.gz | tar xf - --strip-components 1
+wget -O - https://github.com/TechSmith/mp4v2/archive/2.1.0.tar.gz | tar xfz - --strip-components 1
 ./configure
 make libmp4v2.la
 echo "travis_fold:end:mp4v2"
 
 echo "travis_fold:start:faad2"
 mkdir "$AACGAIN_ROOT/faad2" && cd "$AACGAIN_ROOT/faad2"
-wget -O - http://downloads.sourceforge.net/faac/faad2-2.7.tar.bz2 | tar xf - --strip-components 1
+wget -O - http://downloads.sourceforge.net/faac/faad2-2.7.tar.bz2 | tar xfj - --strip-components 1
 ./configure
 cd libfaad
 make
