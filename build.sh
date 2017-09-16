1e4eae9662cb214aa165b925bafc151711839753
David Gilman <davidgilman1@gmail.com>
2017-09-15 20:41:25 -0400

    Add tarball build mode to build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,32 @@
 set -ex
 
 AACGAIN_ROOT=$(pwd)
+AACGAIN_TARBALL=0
+
+while getopts ":t" opt; do
+   case $opt in
+      t) AACGAIN_TARBALL=1
+         ;;
+   esac
+done
 
 echo "travis_fold:start:mp4v2"
 mkdir "$AACGAIN_ROOT/mp4v2" && cd "$AACGAIN_ROOT/mp4v2"
 wget -O - https://github.com/TechSmith/mp4v2/archive/2.1.0.tar.gz | tar xfz - --strip-components 1
-./configure
-make libmp4v2.la
+if [ $AACGAIN_TARBALL -ne 1 ] ; then
+   ./configure
+   make libmp4v2.la
+fi
 echo "travis_fold:end:mp4v2"
 
 echo "travis_fold:start:faad2"
 mkdir "$AACGAIN_ROOT/faad2" && cd "$AACGAIN_ROOT/faad2"
 wget -O - https://downloads.sourceforge.net/faac/faad2-2.7.tar.bz2 | tar xfj - --strip-components 1
-./configure
-cd libfaad
-make
+if [ $AACGAIN_TARBALL -ne 1 ] ; then
+   ./configure
+   cd libfaad
+   make
+fi
 echo "travis_fold:end:faad2"
 
 # delete mpglibDBL/config.h?
@@ -22,9 +34,11 @@ cd "$AACGAIN_ROOT"
 echo "travis_fold:start:aacgain_autoreconf"
 autoreconf -vif
 echo "travis_fold:end:aacgain_autoreconf"
-echo "travis_fold:start:aacgain_configure"
-./configure
-echo "travis_fold:end:aacgain_configure"
-make
+if [ $AACGAIN_TARBALL -ne 1 ] ; then
+   echo "travis_fold:start:aacgain_configure"
+   ./configure
+   echo "travis_fold:end:aacgain_configure"
+   make
 
-"$AACGAIN_ROOT/aacgain/aacgain" -h
+   "$AACGAIN_ROOT/aacgain/aacgain" -h
+fi
